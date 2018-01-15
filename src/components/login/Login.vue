9593cd745a5c0428e3a030a19b761c7cdbb04c7a
lyx <584719733@qq.com>
2018-01-16 06:52:15 +0800

    稍微修改了一下

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -2,20 +2,20 @@
     <div class="login" style="background-image:url(../../static/images/login/login_big.jpg)">
         <div class="login_box">
             <el-form ref="form"  class="login_form" :model="loginForm">
-                <el-form-item label="">
+                <el-form-item label="" @click="addborder($event)">
                     <i class="iconfont icon-zhanghao"></i>
                     <el-input type="text" placeholder="请输入您的账号" v-model="loginForm.name"></el-input>
                 </el-form-item>
-                <el-form-item label="">
+                <el-form-item label="" @click="addBorder()">
                     <i class="iconfont icon-mima"></i>
                     <el-input type="password" placeholder="请输入您的密码"   v-model="loginForm.password"></el-input>
                 </el-form-item>
-                <el-form-item label="" class="code">
+                <el-form-item label="" class="code" @click="addBorder()">
                     <i class="iconfont icon-yanzhengma"></i>
                     <el-input   placeholder="请输入验证码" v-model="loginForm.number"></el-input>
                 </el-form-item>
                 <img class="code_img" src="~static/images/login/6078.jpg" alt="">
-                <el-button class="login_submit">登录</el-button>
+                <el-button class="login_submit" @click="submitForm()">登录</el-button>
             </el-form>
         </div>
     </div>
@@ -26,6 +26,10 @@ export default {
     methods: {
         submitForm() {
             this.$router.push({ path: '/index' });
+        },
+        addborder(e){
+            alert(1)
+            console.log(e)
         }
     },
     data() {
